c5843e0f1b419ecef26a21f7af6445c0eec31e54
Kelly davis <kellyadavis7@gmail.com>
2019-03-07 21:37:31 -0500

    changes for challenge 2

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,11 +10,14 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+// main menu loads upon starting node
 connection.connect(function (err) {
     if (err) throw err;
     runSearch();
 });
 
+
+// main menu
 function runSearch() {
     inquirer
         .prompt({
@@ -22,21 +26,16 @@ function runSearch() {
             message: "What would you like to do?",
             choices: [
                 "View All Products",
-                "Search Products for Sale",
                 "'Last Chance' Products",
                 "Add to Inventory",
                 "Add New Product",
-                "exit"
+                "Exit"
             ]
         })
         .then(function (answer) {
             switch (answer.action) {
                 case "View All Products":
                     selectProducts();
-                    break;
-
-                case "Search Products for Sale":
-                    productSearch();
                     break;
 
                 case "'Last Chance' Products":
@@ -63,28 +62,130 @@ function selectProducts() {
     var query = "SELECT * FROM products;"
     connection.query(query, function (err, res) {
         console.log(res);
-    })
+
+        runSearch();
+    });
 }
 
 // Allows user to search for a product
-function productSearch() {
-    inquirer
-        .prompt({
-            name: "product",
-            type: "input",
-            message: "What product would you like to search for?"
-        })
-        .then(function (answer) {
-            var query = "SELECT product_name, dept_name, price FROM products WHERE ?";
-            connection.query(query, {
-                product_name: answer.product
-            }, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log("Name: " + res[i].product_name + " || Department: " + res[i].dept_name + " || Price: " + res[i].price);
-                }
-                runSearch();
-            });
-        });
-}
+// function productSearch() {
+//     inquirer
+//         .prompt({
+//             name: "product",
+//             type: "input",
+//             message: "What product would you like to search for?"
+//         })
+//         .then(function (answer) {
+//             var query = "SELECT product_name, dept_name, price FROM products WHERE ?";
+//             connection.query(query, {
+//                 product_name: answer.product
+//             }, function (err, res) {
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log("Name: " + res[i].product_name + " || Department: " + res[i].dept_name + " || Price: " + res[i].price);
+//                 }
+//                 runSearch();
+//             });
+//         });
+// }
 
 // finds the lowest inventory items
+function lowSearch() {
+    var query = "SELECT * FROM products WHERE stock < 5";
+    connection.query(query, function (err, res) {
+        console.log(res);
+
+        connection.end();
+
+        // options
+        runSearch();
+
+    });
+}
+
+// update stock quantity via item id 
+function addInventory() {
+    inquirer.prompt([{
+                name: "id",
+                type: "input",
+                message: "Enter the ID of an item to add more of:",
+                filter: Number
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many to add?",
+                filter: Number
+            }
+        ])
+        .then(function (input) {
+            var selected = input.id;
+            var quan = input.quantity;
+
+            var query = 'SELECT * FROM products WHERE ?';
+            connection.query(query, {
+                id: selected
+            }, function (err, data) {
+                if (err) throw err;
+
+                var thisData = data[0];
+
+                var updated = 'UPDATE products SET stock = ' + (thisData.stock + quan) + ' WHERE id = ' + selected;
+
+                connection.query(updated, function (err, res) {
+                    if (err) throw err;
+
+                    console.log("Successfully updated stock for item with ID " + selected + ". Current stock: " + (thisData.stock + quan));
+                    // connection.end();
+
+                    // present options
+                    runSearch();
+                })
+            })
+        })
+}
+
+// allows manager to add a completely new product to the store
+function newProduct() {
+    inquirer.prompt([{
+                name: "product_name",
+                type: "input",
+                message: "Tell me what the new product is called: "
+            },
+            {
+                name: "dept_name",
+                type: "input",
+                message: "What department is it in?"
+            },
+            {
+                name: "price",
+                type: "input",
+                message: "Price (e.g. 40.25):"
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "Quantity:",
+                filter: Number
+            }
+        ])
+        .then(function (input) {
+            var name = input.product_name;
+            var dept = input.dept_name;
+            var price = input.price;
+            var stock = input.stock;
+
+            // mysql update
+            var query = 'INSERT INTO products SET ?';
+
+            connection.query(query, input, function (error, results, fields) {
+                if (error) throw error;
+
+                console.log("Success!");
+                console.log("Added " + stock + " " + name + "(s) to the " + dept + " department at $" + price + ".");
+
+                connection.end();
+                // show options again
+                runSearch();
+            })
+        })
+}
